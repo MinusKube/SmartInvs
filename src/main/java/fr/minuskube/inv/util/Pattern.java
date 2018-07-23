c4cb9c31110746406f5cccf18621ed59730de93e
Isaac <minuskube@gmail.com>
2018-07-23 21:27:20 +0200

    Add Pattern class #17

---
+++ src/main/java/fr/minuskube/inv/util/Pattern.java
@@ -0,0 +1,46 @@
+package fr.minuskube.inv.util;
+
+import com.google.common.base.Preconditions;
+import fr.minuskube.inv.content.SlotPos;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Pattern<T> {
+
+    private String[] lines;
+    private Map<Character, T> mapping = new HashMap<>();
+
+    public Pattern(String... lines) {
+        Preconditions.checkArgument(lines.length > 0, "The given pattern lines must not be empty.");
+
+        int columnCount = lines[0].length();
+
+        for(int i = 0; i < lines.length; i++) {
+            String line = lines[i];
+
+            Preconditions.checkArgument(line.length() == columnCount,
+                    "The given pattern line %s does not match the first line character count.", i);
+        }
+
+        this.lines = lines;
+    }
+
+    public void attach(char character, T object) {
+        this.mapping.put(character, object);
+    }
+
+    public T getObject(SlotPos slot) { return this.getObject(slot.getRow(), slot.getColumn()); }
+    public T getObject(int row, int column) {
+        Preconditions.checkArgument(row >= 0 && row < lines.length,
+                "The row must be between 0 and the row count");
+        Preconditions.checkArgument(column >= 0 && column < lines[row].length(),
+                "The column must be between 0 and the column count");
+
+        return this.mapping.get(this.lines[row].charAt(column));
+    }
+
+    public int getRowCount() { return this.lines.length; }
+    public int getColumnCount() { return this.lines[0].length(); }
+
+}
