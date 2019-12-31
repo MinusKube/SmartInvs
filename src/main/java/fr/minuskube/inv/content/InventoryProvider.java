a523abd5d48134c31bce95968626ba3d020f7986
Clément P. (Cleymax) <clemperrin1@gmail.com>
2019-12-31 01:01:44 +0100

    Make update a default method #84

--- src/main/java/fr/minuskube/inv/content/InventoryProvider.java
+++ src/main/java/fr/minuskube/inv/content/InventoryProvider.java
@@ -5,6 +5,8 @@ import org.bukkit.entity.Player;
 public interface InventoryProvider {
 
     void init(Player player, InventoryContents contents);
-    void update(Player player, InventoryContents contents);
+    default void update(Player player, InventoryContents contents){
+
+    }
 
 }
