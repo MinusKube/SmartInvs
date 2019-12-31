1ad082ba4ffdbc9da001340863ce96a66bebbedc
Clément P <clemperrin1@gmail.com>
2019-12-31 15:45:04 +0100

    Make InventoryProvider#update(Player, InventoryContents) a default method (#85)
    * Make update a default method #84
    
    * Change formatting of the InventoryProvider#update method
    
    Co-authored-by: MinusKube <minuskube@gmail.com>

--- src/main/java/fr/minuskube/inv/content/InventoryProvider.java
+++ src/main/java/fr/minuskube/inv/content/InventoryProvider.java
@@ -5,6 +5,6 @@ import org.bukkit.entity.Player;
 public interface InventoryProvider {
 
     void init(Player player, InventoryContents contents);
-    void update(Player player, InventoryContents contents);
+    default void update(Player player, InventoryContents contents) {}
 
 }
