f60b623bcac53169a60fb948b074f3a5e4576291
ezenity <anthonymmacallister@gmail.com>
2020-03-27 21:38:59 -0400

    Main class JavaDocs

--- src/main/java/fr/minuskube/inv/SmartInvsPlugin.java
+++ src/main/java/fr/minuskube/inv/SmartInvsPlugin.java
@@ -2,11 +2,20 @@ package fr.minuskube.inv;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * SmartInvs main plugin class.
+ * <p>
+ * @author MinusKube
+ * @version 1.2.7
+ */
 public class SmartInvsPlugin extends JavaPlugin {
 
     private static SmartInvsPlugin instance;
     private static InventoryManager invManager;
 
+    /**
+     * This method is called when the plugin is enabled.
+     */
     @Override
     public void onEnable() {
         instance = this;
@@ -15,7 +24,18 @@ public class SmartInvsPlugin extends JavaPlugin {
         invManager.init();
     }
 
+    /**
+     * Creates an instance of the Inventory Manager class.
+     *
+     * @return inventory manager instance
+     */
     public static InventoryManager manager() { return invManager; }
+
+    /**
+     * Creates an instance of the main SmartInvs plugin class.
+     *
+     * @return SmartInvs instance
+     */
     public static SmartInvsPlugin instance() { return instance; }
 
 }
