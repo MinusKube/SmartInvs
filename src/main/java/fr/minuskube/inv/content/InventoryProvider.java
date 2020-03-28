5295ae690146dd4ee4b362ca2eec9aa6b494f98e
ezenity <anthonymmacallister@gmail.com>
2020-03-27 21:39:52 -0400

    Inventory Provider Interface JavaDocs

--- src/main/java/fr/minuskube/inv/content/InventoryProvider.java
+++ src/main/java/fr/minuskube/inv/content/InventoryProvider.java
@@ -2,9 +2,24 @@ package fr.minuskube.inv.content;
 
 import org.bukkit.entity.Player;
 
+/**
+ * Inventory Provider Interface
+ */
 public interface InventoryProvider {
-
+    /**
+     * Will check for all the contents inside the SmartInvs and get the given player.
+     *
+     * @param player Get the given player
+     * @param contents Get all the contents inside the SmartInvs
+     */
     void init(Player player, InventoryContents contents);
+
+    /**
+     * Runs a hashmap to update the player with the given contents inside the SmartInvs.
+     *
+     * @param player Get given player
+     * @param contents Get all the contents inside the SmartInvs
+     */
     default void update(Player player, InventoryContents contents) {}
 
 }
